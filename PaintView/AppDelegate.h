15bc9b371924735b5076ff48598781beb69facf8
kk <kai_kai03@hotmail.com>
2018-09-18 15:21:12 +0800

    Initial Commit

---
+++ PaintView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PaintView
+//
+//  Created by kk on 2018/9/18.
+//  Copyright © 2018年 kk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
